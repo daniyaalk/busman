8eaedecf3ffdd78ccc9a816b23b9a93d008a4106
Daniyaal Khan <khandaniyaal2000@gmail.com>
2020-10-16 10:00:23 +0530

    Added Register form

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 class UserLoginView(LoginView):
@@ -13,3 +14,17 @@ class UserLoginView(LoginView):
             return redirect('org-dash')
         else:
             return super().get(request, *args, **kwargs)
+
+def register(request):
+    
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            return redirect('org-dash')
+
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'users/register.html', {'form': form})
