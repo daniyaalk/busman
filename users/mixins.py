657476b9040f8d5552fa145d578e850e8890f9bc
Daniyaal Khan <khandaniyaal2000@gmail.com>
2020-10-28 17:02:12 +0530

    Added Mixin for testing permissions

---
+++ users/mixins.py
@@ -0,0 +1,16 @@
+from django.core.exceptions import PermissionDenied
+from .models import Permissions
+
+class PermissionsHandlerMixin:
+
+    def dispatch(self, request, *args, **kwargs):
+
+        if hasattr(self, 'permissions_required') and hasattr(self, 'permissions_level'):
+
+            for i, permission in enumerate(self.permissions_required):
+                if getattr(self.request.user.permissions, permission) < self.permissions_level[i]:
+                    print("here")
+                    raise PermissionDenied
+
+        return super().dispatch(request, *args, **kwargs)
+
