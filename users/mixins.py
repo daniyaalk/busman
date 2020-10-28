c87f5f33fe976dee52ba8738e44b69d88ca8d575
Daniyaal Khan <khandaniyaal2000@gmail.com>
2020-10-28 17:33:43 +0530

    Ignore permissions if logged in user is the owner.

--- users/mixins.py
+++ users/mixins.py
@@ -8,8 +8,8 @@ class PermissionsHandlerMixin:
         if hasattr(self, 'permissions_required') and hasattr(self, 'permissions_level'):
 
             for i, permission in enumerate(self.permissions_required):
-                if getattr(self.request.user.permissions, permission) < self.permissions_level[i]:
-                    print("here")
+                is_owner = hasattr(self.request.user, 'organization')
+                if not is_owner and getattr(self.request.user.permissions, permission) < self.permissions_level[i]:
                     raise PermissionDenied
 
         return super().dispatch(request, *args, **kwargs)
